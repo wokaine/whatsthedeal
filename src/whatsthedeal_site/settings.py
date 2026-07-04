5f703d9bc7f4de4aade7d02c37d52b59397c3ff1
Freddie Butterfield <cookies.fo2@gmail.com>
2026-07-04 10:40:28 +0000

    static url

--- src/whatsthedeal_site/settings.py
+++ src/whatsthedeal_site/settings.py
@@ -157,8 +157,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
